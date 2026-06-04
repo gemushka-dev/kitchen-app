76764d1a43d34b89b40bd4ef0daef0ec5c147928
unknown <iskandar.django.work@gmail.com>
2026-06-04 15:10:19 +0500

    refactor: add logic response

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -9,13 +9,13 @@ export async function POST(request: NextRequest) {
     const { email, password, username }: UserDTOType = body;
     if (!email || !password || !username)
       return NextResponse.json(
-        { error: "Missing email or password or username" },
+        { success: false, error: "Missing email or password or username" },
         { status: 400 },
       );
     const registeredUser = await getUserByEmail(email);
     if (registeredUser)
       return NextResponse.json(
-        { error: "User already registered" },
+        { success: false, error: "User already registered" },
         { status: 409 },
       );
     const hashedPassword = await hash(password, 10);
@@ -26,16 +26,16 @@ export async function POST(request: NextRequest) {
     });
     if (!user)
       return NextResponse.json(
-        { error: "Internal Server Error" },
+        { success: false, error: "Internal Server Error" },
         { status: 500 },
       );
     return NextResponse.json(
-      { message: "Successfully registred" },
+      { success: true, message: "Successfully registred" },
       { status: 200 },
     );
   } catch (e) {
     return NextResponse.json(
-      { error: "Internal Server Error" },
+      { success: false, error: "Internal Server Error" },
       { status: 500 },
     );
   }
